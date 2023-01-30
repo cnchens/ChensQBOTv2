41d765d8bff36143ac4c4173bd851ca21560d570
Chens <1836178267@qq.com>
2023-01-30 22:32:26 +0800

    Update sysinfo.py

--- src/plugins/admincommands/rootuser/sysinfo.py
+++ src/plugins/admincommands/rootuser/sysinfo.py
@@ -1,6 +1,4 @@
 from nonebot.plugin.on import on_command
-from nonebot.adapters import Message
-from nonebot.params import EventMessage
 from nonebot.adapters.onebot.v11.event import GroupMessageEvent
 import pymongo
 import json
@@ -9,6 +7,7 @@ import pytz
 import psutil
 import platform
 import wmi
+import time
 
 f = open('src/config/chensbot_config.json', 'r', encoding='utf-8')# 读取config
 json_res = json.load(f)
@@ -17,12 +16,11 @@ mdb_conn = json_res['mdb_conn']# mongodb连接地址
 client = pymongo.MongoClient(mdb_conn)# mongodb连接地址
 db = client['ChensBOTv2']
 
-config_col = db['cb_config']
 rootuser_col = db['admin_rootuser']
 
-sysinfo = on_command('sysinfo')
+matcher = on_command('sysinfo')
 
-@sysinfo.handle()
+@matcher.handle()
 async def _(event: GroupMessageEvent):
     request_qid = str(event.user_id)
     isrootuser = False
@@ -35,10 +33,7 @@ async def _(event: GroupMessageEvent):
             pass
     if isrootuser == True:
         try:
-            mdbtz_time = ''
-            for i in config_col.find():
-                timezone = i['timezone']
-                mdbtz_time = datetime.datetime.now(tz=pytz.timezone(timezone)).strftime('%Y-%m-%d %H:%M:%S')
+            ostime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
             cpuinfo = wmi.WMI()
             for cpu in cpuinfo.Win32_Processor():# cpu使用
                 cpuname = cpu.Name
@@ -50,10 +45,8 @@ async def _(event: GroupMessageEvent):
             total = str(round(psutil.virtual_memory().total / (1024.0 * 1024.0 * 1024.0), 2))+'GB'
             # 操作系统
             osinfo = platform.platform()
-            await sysinfo.send(
+            await matcher.send(
 f'''
-MongoDB设置时区：{timezone}
-当前时间：{mdbtz_time}
 CPU：{cpuname}
 物理核心：{cpucore}
 逻辑核心：{cpulogcore}
@@ -61,9 +54,10 @@ CPU使用：{cpuload}
 总内存：{total}
 可用内存：{free}
 操作系统：{osinfo}
+系统时间：{ostime}
 '''.strip()
             )
         except:
-            await sysinfo.send('错误：调用失败')
+            await matcher.send('错误：调用失败')
     else:
-        await sysinfo.send('错误：权限不足')
+        await matcher.send('错误：权限不足')
